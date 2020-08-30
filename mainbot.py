cd6e1b7c16e42c331bdee601774def2935c97b35
ameenmanna8824 <52666434+ameenmanna8824@users.noreply.github.com>
2020-08-30 23:27:06 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -1,14 +1,15 @@
 import tweepy
 import time
+inport os
 
 print('HII, I am INVERSO BOT')
 #Authenticate to Twitter
-CONSUMER_KEY = 'rDT7cwwQLTaIzeGJJ5EwE9eNu'
-CONSUMER_SECRET = 'VXhRk6HKXr9nXrfWCTVdZBugOobD5KwZwZjZYSXghaG9o9AJ0w'
-ACCESS_KEY = '1299783980783288320-9sj0K3TrsJYDs94dsGl3546mOC1Tme'
-ACCESS_SECRET = 'k4afSrHPElDjqhP3ucvZo5UXTLPDMT0auXqCJScHAIap1'
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+ACCESS_KEY = os.getenv('ACCESS_KEY')
+ACCESS_SECRET = os.getenv('ACCESS_SECRET')
 
-#Setting up connection
+
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
@@ -16,7 +17,7 @@ api.verify_credentials()
 print('API Created')
 
 user= api.me()
-search = '#womenwhocode'
+search = '#womenwhocode','#100DaysOfCode','#Python3','#diazoniclabs'
 num_tweet = 350
 
 #fetching tweets

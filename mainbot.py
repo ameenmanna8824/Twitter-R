b310231b5bc8939f2bd6ac365586af8ef0bcc8ae
ameenmanna8824 <52666434+ameenmanna8824@users.noreply.github.com>
2020-08-30 23:20:06 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -1,23 +1,22 @@
-from __future__ import unicode_literals
 import tweepy
 import time
-import os
 
-print('Hi, I am INVERSO BOT')
+print('HII, I am INVERSO BOT')
 #Authenticate to Twitter
-
-CONSUMER_KEY = os.getenv('CONSUMER_KEY')
-CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
-ACCESS_KEY = os.getenv('ACCESS_KEY')
-ACCESS_SECRET = os.getenv('ACCESS_SECRET')
+CONSUMER_KEY = 'rDT7cwwQLTaIzeGJJ5EwE9eNu'
+CONSUMER_SECRET = 'VXhRk6HKXr9nXrfWCTVdZBugOobD5KwZwZjZYSXghaG9o9AJ0w'
+ACCESS_KEY = '1299783980783288320-9sj0K3TrsJYDs94dsGl3546mOC1Tme'
+ACCESS_SECRET = 'k4afSrHPElDjqhP3ucvZo5UXTLPDMT0auXqCJScHAIap1'
 
 #Setting up connection
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+api.verify_credentials()
+print('API Created')
 
 user= api.me()
-search = '#diazonic'
+search = '#womenwhocode'
 num_tweet = 350
 
 #fetching tweets
@@ -35,4 +34,3 @@ for tweet in tweepy.Cursor(api.search, search).items(num_tweet):
 	except StopIteration:
 		break
 
-         
